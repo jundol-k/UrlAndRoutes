5d78f39693c530c7dfaa6bcadf882928ed3abbd5
= <=>
2019-12-11 17:26:34 +0900

    init

---
+++ UrlAndRoutes.Tests/RouteTests.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Reflection;
+using System.Web;
+using System.Web.Routing;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+
+namespace UrlAndRoutes.Tests
+{
+    [TestClass]
+    public class RouteTests
+    {
+        private HttpContextBase CreateHttpContext(string targetUrl = null, string httpMethod = "GET")
+        {
+            // Mock Request 개체 생성
+            Mock<HttpRequestBase> mockRequest = new Mock<HttpRequestBase>();
+            // HttpRequestBase.AppRelativeCurrentExecutionFilePath 속성을 이용해서 테스트하려는 URL을 노출한다.
+            mockRequest.Setup(m => m.AppRelativeCurrentExecutionFilePath).Returns(targetUrl);
+            mockRequest.Setup(m => m.HttpMethod).Returns(httpMethod);
+
+            // Mock Response 개체 생성
+            Mock<HttpResponseBase> mockResponse = new Mock<HttpResponseBase>();
+            mockResponse.Setup(m => m.ApplyAppPathModifier(It.IsAny<string>())).Returns<string>(s => s);
+
+            // request 개체와 response 개체를 사용하는 mock 컨텍스트 개체 생성
+            Mock<HttpContextBase> mockContext = new Mock<HttpContextBase>();
+            mockContext.Setup(m => m.Request).Returns(mockRequest.Object);
+            mockContext.Setup(m => m.Response).Returns(mockResponse.Object);
+
+            // Mock 컨텍스트 개체 반환
+            return mockContext.Object;
+        }
+
+        // 라우트를 테스트해주는 메서드
+        private void TestRouteMatch(string url, string controller, string action, object routeProperties = null, string httpMethod = "GET")
+        {
+            // Arrange
+            RouteCollection routes = new RouteCollection();
+            RouteConfig.RegisterRoutes(routes);
+            // Act
+            RouteData result = routes.GetRouteData(CreateHttpContext(url, httpMethod));
+
+            // Assert
+            Assert.IsNotNull(result);
+            Assert.IsTrue(TestIncomingRouteResult(result, controller, action, routeProperties));
+        }
+
+        private bool TestIncomingRouteResult(RouteData routeResult, string controller, string action, object propertySet = null)
+        {
+            Func<object, object, bool> valCompare = (v1, v2) =>
+            {
+                return StringComparer.InvariantCultureIgnoreCase.Compare(v1, v2) == 0;
+            };
+
+            bool result = valCompare(routeResult.Values["controller"], controller) && valCompare(routeResult.Values["action"], action);
+
+            if (propertySet != null)
+            {
+                PropertyInfo[] propInfo = propertySet.GetType().GetProperties();
+                foreach (PropertyInfo pi in propInfo)
+                {
+                    if (!(routeResult.Values.ContainsKey(pi.Name) && valCompare(routeResult.Values[pi.Name], pi.GetValue(propertySet, null))))
+                    {
+                        result = false;
+                        break;
+                    }
+                }
+            }
+            return result;
+        }
+
+        // 비정상적인 URL은 처리되지 않도록 검사하는 메서드
+        private void TestRouteFail(string url)
+        {
+            // Arrange
+            RouteCollection routes = new RouteCollection();
+            RouteConfig.RegisterRoutes(routes);
+            // Act
+            RouteData result = routes.GetRouteData(CreateHttpContext(url));
+            // Assert
+            Assert.IsTrue(result == null || result.Route == null);
+        }
+
+        [TestMethod]
+        public void TestIncomingRoutes()
+        {
+            // 기대하고 있는 URL을 검사한다.
+            TestRouteMatch("~/Admin/Index", "Admin", "Index");
+            TestRouteMatch("~/One/Two", "One", "Two");
+
+            // 세그먼트가 너무 많거나 적으면 매치가 실패한다.
+            TestRouteFail("~/Admin/Index/Segment");
+            TestRouteFail("~/Admin");
+        }
+    }
+}
